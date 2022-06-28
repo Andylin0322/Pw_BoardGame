b964c192ff0c1966561b4c3d114ca65b3b86090d
Andylin0322 <74803297+Andylin0322@users.noreply.github.com>
2022-06-28 19:58:31 +0800

    Add files via upload
    bap

---
+++ main.js
@@ -0,0 +1,379 @@
+const Quizdata = [
+  {
+    questions: "Who used the YMCA building in WW2?",
+    options: ["British Intelligence", "Kempeitai", "Resistance fighters"],
+    ans: 1,
+  },
+  {
+    questions: "What was the name of the war heroine tortured in the YMCA building?",
+    options: ['Elizabeth Choy', 'Bertha Hendricks', 'Choy Siew May'],
+    ans: 0,
+  },
+  {
+    questions: "Where is the YMCA building located?",
+    options: ['Orchard Road', 'Bukit Timah Road', 'Jurong'],
+    ans: 0,
+  },
+  {
+    questions: "What were people imprisoned in the YMCA for?",
+    options: ['Anti-japanese dissidence', 'Espionage', 'Disrespect of the Japanese Flag'],
+    ans: 0,
+  },
+  {
+    questions: "What is the YMCA building used for now?",
+    options: ['Car park', 'Shopping mall', 'YMCA HQ'],
+    ans: 2,
+  },
+  {
+    questions: "What is the capacity of the old Cathay cinema?",
+    options: ['1300', '1000', '2000'],
+    ans: 0,
+  },
+  {
+    questions: "What is the name of the director who lived in the old Cathay cinema?",
+    options: ['Akira Kurosawa', 'Nobunaga Oda', 'Yasujiro Ozu'],
+    ans: 2,
+  },
+  {
+    questions: "Which Indian hero used the old Cathay cinema?",
+    options: ['Subhas Chandra Bose', 'Indira Gandhi', 'Mahatma Gandhi'],
+    ans: 0,
+  },
+  {
+    questions: "What did the height of the old Cathay cinema allow the Japanese to do?",
+    options: ['Use it as a watchtower', 'Broadcast propaganda', 'Hold prisoners'],
+    ans: 1,
+  },
+  {
+    questions: "When was the new Cathay cinema opened?",
+    options: ['2003', '2001', '2006'],
+    ans: 2,
+  },
+  {
+    questions: "What was the name of the leader who led the Operation Rimau?",
+    options: ['Ivan Lyon', 'Arthur Percival', 'Bomber Harris'],
+    ans: 0,
+  },
+  {
+    questions: "What previous operation preceded Operation Rimau?",
+    options: ['Operation Sealion', 'Operation Barbarossa', 'Operation Jaywick'],
+    ans: 2,
+  },
+  {
+    questions: "What were the names of the mini submarines Force Z was supposed to deploy during Operation Rimau? ",
+    options: ['Sleeping Beauties', 'U boats', 'Limpets '],
+    ans: 0,
+  },
+  {
+    questions: "What was the name of the junk Force Z hijacked during Operation Rimau?",
+    options: ['Porpoise', 'Mustika', 'Tantalus'],
+    ans: 1,
+  },
+  {
+    questions: "How were the captured British spies killed after Operation Rimau?",
+    options: ['Decapitation', 'Firing squad', 'Hanging'],
+    ans: 0,
+  },
+  {
+    questions: "Where is the Alexandra hospital located?",
+    options: ['Queenstown', 'Jurong', 'Yishun'],
+    ans: 0,
+  },
+  {
+    questions: "Which directions were the Allies retreating from Alexandra Hospital?",
+    options: ['North', 'West', 'South'],
+    ans: 2,
+  },
+  {
+    questions: "What was the nickname for the Alexandra Hospital?",
+    options: ['The Alex', 'Old Alex', 'Mother Alex'],
+    ans: 0,
+  },
+  {
+    questions: "How many Allied nurses and troops were led out of the hospital by the Japanese?",
+    options: ['300', '200', '100'],
+    ans: 1,
+  },
+  {
+    questions: "How were theose who were led out and survived the night treated the next morning?",
+    options: ['Rescued by the British Troops', 'Taken hostage by the Japanese', 'Bayonetted to death'],
+    ans: 2,
+  },
+  {
+    questions: "Where is the Old Ford Factory located?",
+    options: ['Orchard Road', 'Bukit Timah', 'Yishun'],
+    ans: 1,
+  },
+  {
+    questions: "What is the style of architecture used on the Old Ford factory?",
+    options: ['Art Deco', 'Baroque', 'Victorian'],
+    ans: 0,
+  },
+  {
+    questions: "What time did Percival surrender to the Japanese?",
+    options: ['6.15 am', '6.05 pm', '5.15 pm'],
+    ans: 2,
+  },
+  {
+    questions: "How many Commonwealth soldiers were killed during the fall of Singapore?",
+    options: ['5000', '3000', '4000'],
+    ans: 0,
+  },
+  {
+    questions: "How tall is the Hwa Chong Clock Tower",
+    options: ['31 m', '30 m', '20 m'],
+    ans: 0,
+  },
+  {
+    questions: "When was the Hwa Chong Clock Tower built?",
+    options: ['1925', '1920', '1935'],
+    ans: 0,
+  },
+  {
+    questions: "Which architectural firm built the Hwa Chong Clock Tower?",
+    options: ['I.M. Pei and co.', 'Swan & Maclaren', 'Skyline Ltd.'],
+    ans: 1,
+  },
+  {
+    questions: "Why did the Japanese use the Hwa Chong Clock Tower as a strategic base?",
+    options: ['It contained ammo stockpiles', "It was in the centre of Singapore's Administrative District", 'It commanded a good view of the island'],
+    ans: 2,
+  },
+  {
+    questions: "How many faces does the clock on the Hwa Chong Clock Tower have?",
+    options: ['5', '4', '3'],
+    ans: 1,
+  },
+  {
+    questions: "Why did the Japanese invade Bukit Timah?",
+    options: ['To take the resources of food and water in the region', "To capture Percival's right-hand man", 'To destroy British artillery in the region'],
+    ans: 0,
+  },
+  {
+    questions: "Of the 200 Australian Soldiers who were ordered to retake the Jurong Kranji Switch Line, how many of them survived?",
+    options: ['50', '40', '30'],
+    ans: 1,
+  },
+  {
+    questions: "Where did the Commonwealth Forces flee to in the Battle of Bukit Timah?",
+    options: ['Tengah Airfield', 'Ulu Pandan Sleepy Valley Rubber Estate', 'Fort Canning'],
+    ans: 1,
+  },
+  {
+    questions: "Where were the bodies of exhumed Commonwealth soldiers NOT buried? (Battle of Bukit Timah)",
+    options: ['Australia', 'Bukit Timah Military Cemetery', 'Kranji Cemetery'],
+    ans: 1,
+  },
+  {
+    questions: "What kind of buildings surround Bukit Timah today?",
+    options: ['MRT Stations', 'Hawker Centres', 'HDB Buildings'],
+    ans: 2,
+  },
+  {
+    questions: "What were the Japanese's aims in establishing a 2nd beachhead on Kranji Beach? ",
+    options: ['To push southwards into the heart of Singapore', "To capture Percival's top generals", 'To free Japanese POWs captured by the British'],
+    ans: 0,
+  },
+  {
+    questions: "Which of the below were NOT challenges faced by the Japanese in the lead up to the Kranji Beach Battle?",
+    options: ['They had to wade through mangrove forests in full battle-gear', 'They were low on ammo and grenades', 'They faced difficulty in getting their small boats onto choppy waters'],
+    ans: 1,
+  },
+  {
+    questions: "Why did the Japanese sustain heavy casualties during the Kranji Beach Battle?",
+    options: ['They were burnt alive by oil barrels that had been set on fire', 'They were unfamiliar with the terrain and fell into pot-holes', 'They were ambushed by British-Malay regiments who had adopted guerilla techniques'],
+    ans: 0,
+  },
+  {
+    questions: "Which one of the locations below was NOT meant to be defended by the Jurong-Kranji Switch Line in the Kranji Beach Battle?",
+    options: ['Reservoirs', 'Kallang Airfield', 'Sarimbun Beach'],
+    ans: 2,
+  },
+  {
+    questions: "Why did the Jurong-Kranji Switch Line collapse?",
+    options: ['The Japanese broke through the left flank of the 22nd Australian Brigade', "Percival's orders were misinterpreted as an immediate withdrawal", 'British troops were needed in the West to defend against other Japanese offensives'],
+    ans: 1,
+  },
+]
+
+for (y = 0; y < 10; y++) {
+  if (y % 2 == 1) {
+    for (x = 9; x >= 0; x--) {
+      var board = document.createElement('div');
+      var index = 100 - (x + y * 10);
+      board.id = index;
+      if ((x + y) % 2 == 0) {
+        board.className = "dsquare";
+      } else {
+        board.className = "lsquare";
+      }
+      document.getElementById('board').appendChild(board);
+      var text = document.createElement('h2');
+      text.innerHTML = index;
+      document.getElementById(index).appendChild(text);
+    }
+  } else {
+    for (x = 0; x < 10; x++) {
+      var board = document.createElement('div');
+      var index = 100 - (x + y * 10);
+      board.id = index;
+      if ((x + y) % 2 == 1) {
+        board.className = "dsquare";
+      } else {
+        board.className = "lsquare";
+      }
+      document.getElementById('board').appendChild(board);
+      var text = document.createElement('h2');
+      text.innerHTML = index;
+      document.getElementById(index).appendChild(text);
+    }
+  }
+}
+
+currentQ = 0;
+
+let button1 =  document.getElementById("opt1");
+let button2 =  document.getElementById("opt2");
+let button3 =  document.getElementById("opt3");
+optButtons = [button1,button2,button3];
+buttonColors = ["red", "blue", "green", "yellow"];
+
+const newQuestion = () => {
+  currentQ = Math.floor(Math.random()*29);
+  qTag = document.getElementById("questionTag");
+  qTag.innerHTML = Quizdata[currentQ].questions;
+  const usedColors = [];
+  var choice = 0;
+  for(let i=0;i<3;i++){
+    choice = Math.floor(Math.random()*4);
+    var step = 0;
+    while(usedColors.indexOf(choice)!==-1){
+      console.log("choice: "+choice);
+      choice = Math.floor(Math.random()*4);
+    }
+    console.log(choice);
+    usedColors.push(choice);
+    for(let i=0;i<usedColors.length;i++){
+      console.log("list " + i+" "+usedColors[i]);
+    }
+    optButtons[i].className = buttonColors[choice];
+    console.log(choice);
+    optButtons[i].innerHTML = Quizdata[currentQ].options[i];
+  }
+}
+
+const check = (choice) => {
+  console.log(choice);
+  for(let i=0;i<3;i++){
+    if(i==Quizdata[currentQ].ans){
+      optButtons[i].className = buttonColors[2];
+    } else {
+      optButtons[i].className = buttonColors[0];
+    }
+  }
+  if(choice == Quizdata[currentQ].ans){
+    piece1.player_move(false);
+  } else {
+    piece1.player_move(true);
+  }
+}
+
+
+class Pawn {
+  constructor(color, xDisplace, yDisplace, name) {
+    this.piece = document.createElement('div');
+    this.pName = name;
+
+    this.xdis = xDisplace;
+    this.ydis = yDisplace;
+    this.squareIndex = 1;
+    this.xpos = this.squareIndex;
+    this.color = color;
+    this.piece.style.background = this.color;
+
+    this.piece.className = "player";
+    this.piece.style.position = "relative";
+
+    document.getElementById('board').appendChild(this.piece);
+  }
+
+  set_pos() {
+    this.piece.style.top = ((Math.floor((100 - this.squareIndex) / 10) * 4.5) + this.ydis) + "vw";
+    this.xpos = this.squareIndex;
+
+    while (this.xpos > 20) {
+      this.xpos -= 20;
+    }
+    if (this.xpos < 11) {
+      this.xpos -= 1;
+      this.xpos *= 4.5;
+    } else {
+      this.xpos -= 11;
+      this.xpos *= 4.5;
+      this.xpos = 40.5 - this.xpos;
+    }
+    this.xpos += this.xdis;
+    this.piece.style.left = this.xpos + "vw";
+  }
+
+  make_move(move, mistake) {
+    var status = document.getElementById("status");
+    if (mistake==false) {
+      this.squareIndex += move;
+      if (this.squareIndex >= 100) {
+        this.squareIndex = 100;
+      }
+      this.set_pos();
+    }
+    if(this.pName=="Green"){
+      setTimeout(function() {status.innerHTML = "Yellow is making a move..."}, 0);
+    } else if(this.pName=="Yellow"){
+      setTimeout(function() {status.innerHTML = "Red is making a move..."}, 0);
+    } else if(this.pName=="Red"){
+      setTimeout(function() {status.innerHTML = "Blue is making a move..."}, 0);
+    } else if(this.pName=="Blue"){
+      setTimeout(function() {status.innerHTML = "Your turn!"}, 0);
+    }
+  }
+
+  player_move(pMistake) {
+    document.getElementById("opt1").setAttribute('disabled', 'disabled');
+    document.getElementById("opt2").setAttribute('disabled', 'disabled');
+    document.getElementById("opt3").setAttribute('disabled', 'disabled');
+    this.make_move(Math.floor(Math.random() * 6) + 1, pMistake);
+
+
+    const times = [1000 + Math.random() * 4, 1000 * 2 + Math.random() * 4, 1000 * 3 + Math.random() * 4];
+    const moves = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+    const mistakes = [false, false, false];
+
+    for (let i = 0; i < 3; i++) {
+      if(Math.random()>0.3){
+        mistakes[i]=false;
+      } else {
+        mistakes[i]=true;
+      }
+      setTimeout(function() {
+        nonPlayer[i].make_move(moves[i], mistakes[i])
+      }, times[i]);
+    }
+    setTimeout(function() {
+      document.getElementById("opt1").removeAttribute('disabled');
+      document.getElementById("opt2").removeAttribute('disabled');
+      document.getElementById("opt3").removeAttribute('disabled');
+      newQuestion();
+    }, times[2]);
+  }
+}
+
+newQuestion();
+var piece1 = new Pawn("#36AE7C", 0, 0, "Green");
+var piece2 = new Pawn("#F9D923", 0, 0, "Yellow");
+var piece3 = new Pawn("#EB5353", 2.25, -4.5, "Red");
+var piece4 = new Pawn("#187498", 2.25, -4.5, "Blue");
+const nonPlayer = [piece2, piece3, piece4];
+
+piece1.set_pos();
+piece2.set_pos();
+piece3.set_pos();
+piece4.set_pos();
